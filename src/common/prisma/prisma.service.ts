1b529cee5a09a1ef5492bd71fa2e0ee9a0f053b4
Denovo <vo0v0000@naver.com>
2024-04-09 23:56:41 +0900

    feat: prisma 적용

---
+++ src/common/prisma/prisma.service.ts
@@ -0,0 +1,30 @@
+import { INestApplication, Injectable, OnModuleInit } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({
+      log: [
+        {
+          emit: 'stdout',
+          level: process.env.NODE_ENV === 'PRODUCTION' ? 'warn' : 'query',
+        },
+      ],
+      errorFormat: 'pretty',
+    });
+  }
+
+  async onModuleInit() {
+    await this.$connect();
+
+    // 미들웨어
+    // 미들웨어 설정
+  }
+
+  async enableShutdownHooks(app: INestApplication) {
+    process.on('beforeExit', async () => {
+      await app.close();
+    });
+  }
+}
