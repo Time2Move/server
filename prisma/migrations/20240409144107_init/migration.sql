791c3ae8b24ea80d64eaae9739a1c385f04aa652
Denovo <vo0v0000@naver.com>
2024-04-10 00:00:35 +0900

    chore: 버전 및 db migration

--- prisma/migrations/20240409144107_init/migration.sql
+++ prisma/migrations/20240409144107_init/migration.sql
@@ -5,6 +5,15 @@ CREATE TYPE "TERMS_TYPE" AS ENUM ('SERVICE', 'PRIVACY', 'AGE_LIMIT', 'MARKETING'
 CREATE TYPE "OAUT_PROVIDER" AS ENUM ('GOOGLE', 'KAKAO', 'NAVER');
 
 -- CreateEnum
+CREATE TYPE "CERTIFICATION_TARGET_TYPE" AS ENUM ('PHONE');
+
+-- CreateEnum
+CREATE TYPE "CERTIFICATION_TYPE" AS ENUM ('SIGN_UP', 'FIND_PASSWORD');
+
+-- CreateEnum
+CREATE TYPE "CERTIFICATION_STATUS" AS ENUM ('PENDING', 'VERIFIED', 'SUCCESS', 'EXPIRED');
+
+-- CreateEnum
 CREATE TYPE "PARKING_STATUS" AS ENUM ('PARKING', 'COMPLETE');
 
 -- CreateEnum
@@ -90,7 +99,34 @@ CREATE TABLE "oauth" (
 );
 
 -- CreateTable
-CREATE TABLE "Car" (
+CREATE TABLE "certification" (
+    "id" UUID NOT NULL,
+    "user_id" UUID NOT NULL,
+    "targetType" "CERTIFICATION_TARGET_TYPE" NOT NULL,
+    "type" "CERTIFICATION_TYPE" NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "certification_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "certification_code" (
+    "id" UUID NOT NULL,
+    "targetType" "CERTIFICATION_TARGET_TYPE" NOT NULL,
+    "type" "CERTIFICATION_TYPE" NOT NULL,
+    "status" "CERTIFICATION_STATUS" NOT NULL,
+    "code" TEXT NOT NULL,
+    "target" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+    "expired_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "certification_code_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "car" (
     "id" UUID NOT NULL,
     "owner_id" UUID NOT NULL,
     "type" TEXT NOT NULL,
@@ -98,22 +134,22 @@ CREATE TABLE "Car" (
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "Car_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "car_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "CarImage" (
+CREATE TABLE "car_image" (
     "id" UUID NOT NULL,
     "car_id" UUID NOT NULL,
     "image_id" UUID NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "CarImage_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "car_image_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "CarSnapshot" (
+CREATE TABLE "car_snapshot" (
     "id" UUID NOT NULL,
     "car_id" UUID NOT NULL,
     "owner_id" UUID NOT NULL,
@@ -122,7 +158,7 @@ CREATE TABLE "CarSnapshot" (
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "CarSnapshot_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "car_snapshot_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -154,7 +190,7 @@ CREATE TABLE "parking" (
 );
 
 -- CreateTable
-CREATE TABLE "ParkingSnapshot" (
+CREATE TABLE "parking_snapshot" (
     "id" UUID NOT NULL,
     "car_id" UUID NOT NULL,
     "drivingId" UUID NOT NULL,
@@ -166,11 +202,11 @@ CREATE TABLE "ParkingSnapshot" (
     "end_date" TIMESTAMP(3) NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    CONSTRAINT "ParkingSnapshot_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "parking_snapshot_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "Notification" (
+CREATE TABLE "notification" (
     "id" UUID NOT NULL,
     "user_id" UUID NOT NULL,
     "type" "NOTIFICATION_TYPE" NOT NULL,
@@ -180,24 +216,34 @@ CREATE TABLE "Notification" (
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "Notification_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "notification_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "UserReputation" (
+CREATE TABLE "user_reputation" (
     "id" UUID NOT NULL,
     "user_id" UUID NOT NULL,
     "writer_id" UUID NOT NULL,
-    "type" TEXT NOT NULL,
     "score" INTEGER NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "UserReputation_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "user_reputation_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "Image" (
+CREATE TABLE "user_reputation_reason" (
+    "id" UUID NOT NULL,
+    "user_reputation_id" UUID NOT NULL,
+    "type" "REPUTATION_TYPE" NOT NULL,
+    "score" INTEGER NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "user_reputation_reason_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "image" (
     "id" UUID NOT NULL,
     "user_id" UUID NOT NULL,
     "type" "IMAGE_TYPE" NOT NULL,
@@ -205,7 +251,7 @@ CREATE TABLE "Image" (
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "deleted_at" TIMESTAMP(3),
 
-    CONSTRAINT "Image_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "image_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -233,10 +279,28 @@ CREATE UNIQUE INDEX "oauth_provider_provider_id_key" ON "oauth"("provider", "pro
 CREATE UNIQUE INDEX "oauth_user_id_provider_key" ON "oauth"("user_id", "provider");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "Car_number_key" ON "Car"("number");
+CREATE UNIQUE INDEX "certification_user_id_type_key" ON "certification"("user_id", "type");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "CarSnapshot_number_key" ON "CarSnapshot"("number");
+CREATE INDEX "certification_code_target_idx" ON "certification_code"("target");
+
+-- CreateIndex
+CREATE INDEX "certification_code_target_status_idx" ON "certification_code"("target", "status");
+
+-- CreateIndex
+CREATE INDEX "certification_code_created_at_idx" ON "certification_code"("created_at");
+
+-- CreateIndex
+CREATE INDEX "certification_code_target_created_at_idx" ON "certification_code"("target", "created_at");
+
+-- CreateIndex
+CREATE INDEX "certification_code_expired_at_idx" ON "certification_code"("expired_at");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "car_number_key" ON "car"("number");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "car_snapshot_number_key" ON "car_snapshot"("number");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "parking_car_id_drivingId_key" ON "parking"("car_id", "drivingId");
@@ -254,7 +318,7 @@ ALTER TABLE "user_last_snapshot" ADD CONSTRAINT "user_last_snapshot_snapshot_id_
 ALTER TABLE "user_profile_image" ADD CONSTRAINT "user_profile_image_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "user_profile_image" ADD CONSTRAINT "user_profile_image_image_id_fkey" FOREIGN KEY ("image_id") REFERENCES "Image"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "user_profile_image" ADD CONSTRAINT "user_profile_image_image_id_fkey" FOREIGN KEY ("image_id") REFERENCES "image"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "terms_agreements" ADD CONSTRAINT "terms_agreements_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
@@ -263,37 +327,46 @@ ALTER TABLE "terms_agreements" ADD CONSTRAINT "terms_agreements_user_id_fkey" FO
 ALTER TABLE "oauth" ADD CONSTRAINT "oauth_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Car" ADD CONSTRAINT "Car_owner_id_fkey" FOREIGN KEY ("owner_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "certification" ADD CONSTRAINT "certification_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "CarImage" ADD CONSTRAINT "CarImage_car_id_fkey" FOREIGN KEY ("car_id") REFERENCES "Car"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "certification" ADD CONSTRAINT "certification_id_fkey" FOREIGN KEY ("id") REFERENCES "certification_code"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "CarImage" ADD CONSTRAINT "CarImage_image_id_fkey" FOREIGN KEY ("image_id") REFERENCES "Image"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "car" ADD CONSTRAINT "car_owner_id_fkey" FOREIGN KEY ("owner_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "CarSnapshot" ADD CONSTRAINT "CarSnapshot_car_id_fkey" FOREIGN KEY ("car_id") REFERENCES "Car"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "car_image" ADD CONSTRAINT "car_image_car_id_fkey" FOREIGN KEY ("car_id") REFERENCES "car"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "car_image" ADD CONSTRAINT "car_image_image_id_fkey" FOREIGN KEY ("image_id") REFERENCES "image"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "car_snapshot" ADD CONSTRAINT "car_snapshot_car_id_fkey" FOREIGN KEY ("car_id") REFERENCES "car"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "driving" ADD CONSTRAINT "driving_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "driving" ADD CONSTRAINT "driving_car_id_fkey" FOREIGN KEY ("car_id") REFERENCES "Car"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "driving" ADD CONSTRAINT "driving_car_id_fkey" FOREIGN KEY ("car_id") REFERENCES "car"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "parking" ADD CONSTRAINT "parking_car_id_fkey" FOREIGN KEY ("car_id") REFERENCES "Car"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "parking" ADD CONSTRAINT "parking_car_id_fkey" FOREIGN KEY ("car_id") REFERENCES "car"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "parking" ADD CONSTRAINT "parking_drivingId_fkey" FOREIGN KEY ("drivingId") REFERENCES "driving"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "ParkingSnapshot" ADD CONSTRAINT "ParkingSnapshot_car_id_drivingId_fkey" FOREIGN KEY ("car_id", "drivingId") REFERENCES "parking"("car_id", "drivingId") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "parking_snapshot" ADD CONSTRAINT "parking_snapshot_car_id_drivingId_fkey" FOREIGN KEY ("car_id", "drivingId") REFERENCES "parking"("car_id", "drivingId") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Notification" ADD CONSTRAINT "Notification_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "notification" ADD CONSTRAINT "notification_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "UserReputation" ADD CONSTRAINT "UserReputation_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "user_reputation" ADD CONSTRAINT "user_reputation_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "UserReputation" ADD CONSTRAINT "UserReputation_writer_id_fkey" FOREIGN KEY ("writer_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "user_reputation" ADD CONSTRAINT "user_reputation_writer_id_fkey" FOREIGN KEY ("writer_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "user_reputation_reason" ADD CONSTRAINT "user_reputation_reason_user_reputation_id_fkey" FOREIGN KEY ("user_reputation_id") REFERENCES "user_reputation"("id") ON DELETE CASCADE ON UPDATE CASCADE;
